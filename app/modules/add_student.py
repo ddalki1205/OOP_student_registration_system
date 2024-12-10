dbd12a3379b7bae12a5f9607533cb282a12326f1
ddalki1205 <nikkaanndreacruz1205@gmail.com>
2024-12-10 10:20:16 +0800

    Update add_student.py for gui

--- app/modules/add_student.py
+++ app/modules/add_student.py
@@ -1,24 +1,24 @@
 class AddStudent:
-  def __init__(self, constructor, student_list, path):
+  def __init__(self, constructor, db, path):
     self.constructor = constructor
-    self.student_list = student_list            #The list in student.py => admin.all_students
-    self.path = path                            #THE TXT FILE
+    self.db = db            #The list in student.py => admin.all_students
+    self.path = path        #THE TXT FILE
 
-  def add_student(self, student):               #this deals with adding and writing(don't use before preloading from file)
-    self.student_list.append(student)
+  def func(self, student): #this deals with adding and writing(don't use before preloading from file)
+    self.db.append(student)
     self.write(student)
 
   def create(self, attributes):
     name, age, id, email, phone = attributes
     new_student = self.constructor(name, age, id, email, phone)
-    self.add_student(new_student)
+    self.func(new_student)
 
   def read(self):
     with open(self.path, 'r') as f:
       for line in f:
         attributes = line.strip().split(',')
         student = self.constructor(*attributes)
-        self.student_list.append(student)
+        self.db.append(student)
 
   def write(self, student):
     with open(self.path, 'a') as f:
