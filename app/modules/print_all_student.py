b06b01b9a00ca2be40a18c5ef48fdc49958b9b1d
ddalki1205 <nikkaanndreacruz1205@gmail.com>
2024-12-10 10:23:07 +0800

    Update print_all_student.py for gui

--- app/modules/print_all_student.py
+++ app/modules/print_all_student.py
@@ -1,4 +1,14 @@
 class PrintAllStudents:
-  def print_all_student(self, student_list):
-    for student in student_list:
-      print(student)
+    def __init__(self, path, constructor):
+        self.path = path  
+        self.constructor = constructor 
+
+    def func(self):
+        try:
+            with open(self.path, 'r') as f:
+                for line in f:
+                    attributes = line.strip().split(',')
+                    student = self.constructor(*attributes)  
+                    print(student)  # Print using the __str__ method of StudentInfo
+        except FileNotFoundError:
+            print("No student data file found.")
