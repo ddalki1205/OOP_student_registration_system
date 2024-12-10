ecf599a8b04da9e28424f86cd2bc92588f0d15d9
ddalki1205 <nikkaanndreacruz1205@gmail.com>
2024-12-10 09:11:09 +0800

    Update main.py for gui

--- app/main.py
+++ app/main.py
@@ -4,24 +4,24 @@ from modules.print_all_student import PrintAllStudents
 from modules.search_student import Search
 from modules.student import StudentInfo
 from modules.main_menu import MainMenu
+import tkinter as tk
 
 DB_PATH = "app/student_data.txt"
 
-
-#initialize the classes
 admin = StudentInfo()
-student_list = admin.all_student
+student_db = admin.all_student
 
-add_student_service = AddStudent(StudentInfo, student_list, DB_PATH)
+add_student_service = AddStudent(StudentInfo, student_db, DB_PATH)
 log_in_service = LogIn()
-print_all_service = PrintAllStudents()
-search_service = Search()
-
+print_all_service = PrintAllStudents(DB_PATH, StudentInfo)  
+search_service = Search(DB_PATH, StudentInfo)  
 
 add_student_service.read()
 
-access = log_in_service.log_in(student_list)
+# Log in process
+access = log_in_service.func(student_db)
 
 if access:
-      main_menu = MainMenu(add_student_service, print_all_service.print_all_student, search_service.search_student, access)
-      main_menu.func()
+    root = tk.Tk()
+    main_menu = MainMenu(root, add_student_service, print_all_service.func, search_service.func, access)
+    root.mainloop()
